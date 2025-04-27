ff3ca4d561fad7d8d84945a0d2f6bbf534ad37d5
true42 <bezmaternykh.alexey@gmail.com>
2025-04-27 12:59:59 +0300

    create keyboard date and time

--- bot_disp.py
+++ bot_disp.py
@@ -1,6 +1,8 @@
 import asyncio
 import logging
 import sys
+from datetime import datetime, timedelta
+from idlelib.window import add_windows_to_menu
 from os import getenv
 from dotenv import load_dotenv
 
@@ -8,7 +10,9 @@ from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+
+from utils.utils import last_day_of_month, generate_button
 
 load_dotenv(dotenv_path='config/config.env')
 TOKEN = getenv("BOT_TOKEN")
@@ -16,6 +20,19 @@ TOKEN = getenv("BOT_TOKEN")
 # All handlers should be attached to the Router (or Dispatcher)
 
 dp = Dispatcher()
+
+
+def create_kb(message: Message):
+    kb_list = [
+        [KeyboardButton(text=f"Hello, {html.bold(message.from_user.full_name)}!")]
+    ]
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=kb_list,
+        resize_keyboard=True,
+        one_time_keyboard=True,
+        input_field_placeholder="Воспользуйтесь меню:"
+    )
+    return keyboard
 
 
 @dp.message(CommandStart())
@@ -28,8 +45,10 @@ async def command_start_handler(message: Message) -> None:
     # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
     # method automatically or call API method directly via
     # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
-    # TODO: добавить кнопку "отметить время"
+    # генерировать кнопки с часами. после нажатия убирать кнопку. плюс кнопка назад. чтобы вернутся к датам
+
+    await message.answer('В какое время на следующей неделе ты готов собраться?',
+                         reply_markup=generate_button('text'))
 
 
 @dp.message()
@@ -39,12 +58,29 @@ async def echo_handler(message: Message) -> None:
 
     By default, message handler will handle all message types (like a text, photo, sticker etc.)
     """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+    # TODO: добавить бд с пользователями датами и временем
+    if message.text == 'Готово':
+        await message.answer('Планы построены', reply_markup=ReplyKeyboardRemove())
+        # TODO: кнопка для начала планирования
+        return
+
+    if '.' in message.text and len(message.text.split('.')) == 2:
+        if (int(message.text.split('.')[0]) > last_day_of_month(datetime.now())
+                or 0 > int(message.text.split('.')[1]) > 12):
+            await message.answer('Некорректная дата, воспользуйся кнопками')
+            return
+        message_type = 'date'
+    elif ':' in message.text and len(message.text.split(':')) == 2:
+        if 0 >= int(message.text.split(':')[0]) > 24 or 0 >= int(message.text.split(':')[1]) > 60:
+            await message.answer('Некорректное время, воспользуйся кнопками')
+            return
+        message_type = 'time'
+    else:
+        message_type = 'text'
+
+    # menu_button = generate_button(message_type)
+
+    await message.answer(text='text example', reply_markup=generate_button(message_type))
 
 
 async def main() -> None:
--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 
 def last_day_of_month(any_day):
@@ -7,6 +8,82 @@ def last_day_of_month(any_day):
     # subtracting the number of the current day brings us back one month
     return_date = next_month - timedelta(days=next_month.day)
     return return_date.day
+
+
+def generate_button(type_button, list_hour=None):
+    if list_hour is None:
+        list_hour = []
+    if type_button == 'text':
+        return generate_button_date()
+    elif type_button == 'date':
+        return generate_button_time(list_hour)
+    else:
+        return generate_button_time()
+
+
+def generate_button_time(list_hour=None):
+    hour_list = [[], [], [], [], [], []]
+    max_len_row = 5
+    index_hour = 0
+    for hour in range(9, 33):
+        try:
+            if hour in list_hour:
+                continue
+        except TypeError:
+            pass
+
+        if len(hour_list[index_hour]) == max_len_row:
+            index_hour += 1
+
+        if hour > 23:
+            hour = hour - 24
+
+        hour_list[index_hour].append(
+            KeyboardButton(
+                text=f'{hour:02}:00'
+            )
+        )
+    else:
+        hour_list[index_hour+1].append(
+            KeyboardButton(
+                text=f'Назад'
+            )
+        )
+
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=hour_list,
+        resize_keyboard=True,
+        one_time_keyboard=True,
+        input_field_placeholder="Воспользуйтесь меню:"
+    )
+    return keyboard
+
+
+def generate_button_date():
+    list_date =[[], []]
+    now = datetime.now()
+
+    plus_date = timedelta(days=7-now.weekday())
+    date_begin_next_week = now + plus_date
+    for i in range(7):
+        day = date_begin_next_week + timedelta(days=i)
+        list_date[0].append(
+            KeyboardButton(
+                text=f'{day.day}.{day.month}'
+            )
+        )
+    else:
+        list_date[1].append(KeyboardButton(
+                text='Готово'
+            ))
+
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=list_date,
+        resize_keyboard=True,
+        one_time_keyboard=True,
+        input_field_placeholder="Воспользуйтесь меню:"
+    )
+    return keyboard
 
 
 if __name__ == '__main__':
